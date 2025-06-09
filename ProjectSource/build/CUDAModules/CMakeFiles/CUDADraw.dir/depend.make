f0d7e75451bae3f3e7aacd29657081fe033c0b17
buklinfur <buklinfur@outlook.com>
2025-06-09 13:27:41 +0700

    added update loader & testing out

--- ProjectSource/build/CUDAModules/CMakeFiles/CUDADraw.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CUDADraw.
-# This may be replaced when dependencies are built.
--- ProjectSource/build/UpdateLoader/scripts/update.sh
+++
@@ -1,8 +0,0 @@
-while pgrep -f "executables"; do
-  sleep 1
-done
-
-tar -xzf /tmp/lbm_update.tar.gz -C "$(dirname "$0")/../../"
-rm /tmp/lbm_update.tar.gz
-
-./executables &
